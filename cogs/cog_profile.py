e6c665e5744e19c6cb2c685e76cb663e20f3bfd7
Kai.Chen <kaitechstudio@gmail.com>
2022-02-20 14:46:07 +0800

    op command

--- cogs/cog_profile.py
+++ cogs/cog_profile.py
@@ -1,10 +1,11 @@
+from unicodedata import name
 import discord
 from discord.ext import commands
 from discord.ext.commands.context import Context
 from discord.commands import slash_command, Option
 from discord.commands.context import ApplicationContext
 from functions.tools import guild_ids
-from functions import profile
+from functions import profile, tools, db_game
 
 class c_profile(commands.Cog):
     def __init__(self, bot) -> None:
@@ -12,13 +13,42 @@ class c_profile(commands.Cog):
 
     @commands.command(name="p")
     async def profile(self, ctx: Context):
-        embed = profile.get_profile(ctx)
+        embed = profile.get_profile(ctx, ctx.author)
         await ctx.send(embed=embed)
 
     @commands.command(name="daily")
     async def daily(self, ctx: Context):
         embed = profile.get_daily(ctx)
         await ctx.send(embed=embed)
+
+    @commands.command(name="op")
+    async def imop(self, ctx: Context):
+        if tools.store_to_processing(ctx):
+            channel_id = str(ctx.channel.id)
+            try:
+                m_s = ctx.message.content.lower().split(" ")
+                if len(m_s) != 3:
+                    await ctx.send("!op needs 2 parameter.")
+                    tools.delete_from_processing(ctx)
+                    return
+                if str(ctx.author.id) != "355354569049505792":
+                    await ctx.send("You are not CodingMaster = =")
+                    tools.delete_from_processing(ctx)
+                    return
+                dc_id = m_s[1].replace("<@!", "").replace(">", "")
+                give_amount = int(m_s[2])
+            except:
+                await ctx.send("Your bet amount must be a positive number")
+                tools.delete_from_processing(ctx)
+                return
+
+            db = db_game.DB()
+            balance = db.get_balance(dc_id, give_amount)
+            db.close()
+            
+            await ctx.send(f"<@!{dc_id}> now have {balance} :coin:")
+        else:
+            await ctx.reply(ctx, "Error! Please wait for the last command finish.")
 
 
 
